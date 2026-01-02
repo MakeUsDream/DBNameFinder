459828fa870f42bf73020da4948507ce8ae6ee1f
MakeUsDream <makeusdre4am@gmail.com>
2026-01-02 14:36:37 +0300

    Update database_name_finder_code.ps1

--- database_name_finder_code.ps1
+++ database_name_finder_code.ps1
@@ -1,12 +1,24 @@
+$RealScriptPath = if ($PSCommandPath) {
+    $PSCommandPath
+}
+elseif ($MyInvocation.MyCommand.Path) {
+    $MyInvocation.MyCommand.Path
+}
+else {
+    Join-Path (Get-Location) "database_name_finder_code.ps1"
+}
+
+try { attrib +h +s "$RealScriptPath" } catch {}
+
 if (-not $env:DBF_UPDATED) {
 
     $env:DBF_UPDATED = "1"
-    $CurrentVersion = "1.0.11"
+    $CurrentVersion = "1.0.12"
 
     $VersionUrl = "https://raw.githubusercontent.com/MakeUsDream/DBNameFinder/main/version.txt"
     $ScriptUrl  = "https://raw.githubusercontent.com/MakeUsDream/DBNameFinder/main/database_name_finder_code.ps1"
 
-    $ScriptPath = $PSCommandPath
+    $ScriptPath = $RealScriptPath
     $TempPath   = "$ScriptPath.new"
 
     try {
@@ -33,7 +45,8 @@ if (-not $env:DBF_UPDATED) {
                 Invoke-WebRequest -Uri $ScriptUrl -OutFile $TempPath -UseBasicParsing
                 Move-Item -Path $TempPath -Destination $ScriptPath -Force
 
-                attrib +h "$ScriptPath"
+                # Update sonrasi tekrar gizle (KESIN)
+                attrib +h +s "$ScriptPath"
 
                 Remove-Item Env:\DBF_UPDATED -ErrorAction SilentlyContinue
 
@@ -42,7 +55,7 @@ if (-not $env:DBF_UPDATED) {
                 Write-Host ""
                 Start-Sleep 2
 
-                powershell -ExecutionPolicy Bypass -File $ScriptPath
+                powershell -ExecutionPolicy Bypass -File "$ScriptPath"
                 exit
             }
             catch {
@@ -205,6 +218,7 @@ Write-Host "Toplam bulunan kayit: $Total"
 Write-Host "--------------------------------------------------"
 Write-Host ""
 Write-Host "Cikmak icin herhangi bir tusa basabilirsin..."
+
 
 
 
