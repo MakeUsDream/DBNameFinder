a22a2c803fa1f390b60de5ad07d72c21e8ca80bc
MakeUsDream <makeusdre4am@gmail.com>
2026-01-02 14:41:48 +0300

    Update database_name_finder_code.ps1

--- database_name_finder_code.ps1
+++ database_name_finder_code.ps1
@@ -13,7 +13,7 @@ try { attrib +h +s "$RealScriptPath" } catch {}
 if (-not $env:DBF_UPDATED) {
 
     $env:DBF_UPDATED = "1"
-    $CurrentVersion = "1.0.13"
+    $CurrentVersion = "1.0.14"
 
     $VersionUrl = "https://raw.githubusercontent.com/MakeUsDream/DBNameFinder/main/version.txt"
     $ScriptUrl  = "https://raw.githubusercontent.com/MakeUsDream/DBNameFinder/main/database_name_finder_code.ps1"
@@ -218,6 +218,7 @@ Write-Host "Toplam bulunan kayit: $Total"
 Write-Host "--------------------------------------------------"
 Write-Host ""
 Write-Host "Cikmak icin herhangi bir tusa basabilirsin..."
+
 
 
 
