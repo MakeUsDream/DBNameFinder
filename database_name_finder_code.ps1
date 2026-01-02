2eff5e70103436ba15bc96d0dc4143035b1104e2
MakeUsDream <makeusdre4am@gmail.com>
2026-01-02 03:33:10 +0300

    Update database_name_finder_code.ps1

--- database_name_finder_code.ps1
+++ database_name_finder_code.ps1
@@ -5,7 +5,7 @@ chcp 857 | Out-Null
 if (-not $env:DBF_UPDATED) {
 
     $env:DBF_UPDATED = "1"
-    $CurrentVersion = "1.0.0"
+    $CurrentVersion = "1.0.1"
 
     $VersionUrl = "https://raw.githubusercontent.com/MakeUsDream/DBNameFinder/main/version.txt"
     $ScriptUrl  = "https://raw.githubusercontent.com/MakeUsDream/DBNameFinder/main/database_name_finder_code.ps1"
@@ -220,3 +220,4 @@ Write-Host "--------------------------------------------------"
 Write-Host ""
 Write-Host ""
 Write-Host "Çıkış yapmak için herhangi bir tuşa basabilirsin..."
+
